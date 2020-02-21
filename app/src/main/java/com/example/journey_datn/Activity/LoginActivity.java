2418f52478b8fee0f29ec357cfb535b97673052e
vanquan11 <vanquan11mu@gmail.com>
2020-02-21 20:56:05 +0700

    Initial commit

---
+++ app/src/main/java/com/example/journey_datn/Activity/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.example.journey_datn.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.journey_datn.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
---
+++ app/src/main/java/com/example/journey_datn/fragment/fragment_Journey.java
@@ -0,0 +1,4 @@
+package com.example.journey_datn.fragment;
+
+public class fragment_Journey {
+}
