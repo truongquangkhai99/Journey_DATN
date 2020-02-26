54355f276d3a7194f45e7ed17cb170035fee6b15
vanquan11 <vanquan11mu@gmail.com>
2020-02-26 20:45:38 +0700

    create Model FragmentJourney

--- app/src/main/java/com/example/journey_datn/Activity/MainActivity.java
+++ app/src/main/java/com/example/journey_datn/Activity/MainActivity.java
@@ -1,16 +1,68 @@
 package com.example.journey_datn.Activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.ImageView;
 
+import com.example.journey_datn.Adapter.SectionsPagerAdapter;
 import com.example.journey_datn.R;
+import com.example.journey_datn.fragment.fragment_Journey;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    private SectionsPagerAdapter mSectionsPagerAdapter;
+    private ViewPager mViewPager;
+    private List<Fragment> fragmentList = new ArrayList<>();
+    private BottomNavigationView navigationView;
+    private ImageView img_menu, img_search, img_cloud;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        init();
+        fragmentList.add(new fragment_Journey());
+        mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager(), fragmentList);
+        mViewPager.setAdapter(mSectionsPagerAdapter);
+        navigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                switch (menuItem.getItemId()){
+                    case R.id.menu_journey:
+                        mViewPager.setCurrentItem(0);
+                        return  true;
+                    case  R.id.menu_calendar:
+                        mViewPager.setCurrentItem(1);
+                        return  true;
+                    case  R.id.menu_media:
+                        mViewPager.setCurrentItem(2);
+                        return  true;
+                    case  R.id.menu_atlas:
+                        mViewPager.setCurrentItem(3);
+                        return  true;
+                    case  R.id.menu_today:
+                        mViewPager.setCurrentItem(4);
+                        return  true;
+                }
+                return false;
+            }
+        });
+    }
+
+    private void init(){
+        mViewPager =  findViewById(R.id.viewPager_contain);
+        navigationView = findViewById(R.id.navigation);
+        img_menu = findViewById(R.id.img_menu);
+        img_search = findViewById(R.id.img_search);
+        img_cloud = findViewById(R.id.img_cloud);
     }
 }
--- app/src/main/java/com/example/journey_datn/fragment/fragment_Journey.java
+++ app/src/main/java/com/example/journey_datn/fragment/fragment_Journey.java
@@ -1,4 +1,60 @@
 package com.example.journey_datn.fragment;
 
-public class fragment_Journey {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.journey_datn.Activity.AddDataActivity;
+import com.example.journey_datn.Adapter.AdapterRcvEmpty;
+import com.example.journey_datn.R;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class fragment_Journey extends Fragment {
+
+    private RecyclerView rcvJourney;
+    private AdapterRcvEmpty adapterRcvEmpty;
+    private LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext());
+
+    private FloatingActionButton fabJourney;
+    private TextView txt_user_name, txt_day,txt_month, txt_year, txt_number_item;
+
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_journey, container, false);
+        initView(view);
+        fabJourney.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getContext(), AddDataActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        adapterRcvEmpty = new AdapterRcvEmpty(getContext());
+        rcvJourney.setAdapter(adapterRcvEmpty);
+        rcvJourney.setLayoutManager(linearLayoutManager);
+
+        return view;
+    }
+
+    private void initView(View view) {
+        rcvJourney = view.findViewById(R.id.rcv_journey);
+        txt_user_name = view.findViewById(R.id.txt_user_name);
+        txt_day = view.findViewById(R.id.txt_day);
+        txt_month = view.findViewById(R.id.txt_month);
+        txt_year = view.findViewById(R.id.txt_year);
+        txt_number_item = view.findViewById(R.id.txt_number_item);
+        fabJourney = view.findViewById(R.id.fab_journey);
+    }
 }
